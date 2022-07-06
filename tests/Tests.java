eca39917ea8475b1b07c4c47232941d62407c38e
Lucas Yang <lucas.yang@comcast.net>
2022-07-06 11:43:15 -0400

    Update Tests.java

--- tests/Tests.java
+++ tests/Tests.java
@@ -2,14 +2,26 @@ package tests;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import programs.Bank;
+
+import programs.Account;
 
 public class Tests {
 
 	@Test
-	public void testScanner() {
-		Bank bank = new Bank("Bank of Java");
-		assertEquals(bank.getName(), "Bank of Java");
+	public void testAccountValues() {
+		Account account = new Account("johndoe123@gmail.com", "badpassword123", 0.00);
+		
+		assertEquals(account.getEmail(), "johndoe123@gmail.com");
+		assertEquals(account.getPassword(), "badpassword123");
+		assertTrue(account.getBalance() == 0.00);
+		
+		account.resetPassword("anotherterriblepassword456");
+		assertFalse(account.getPassword().equals("badpassword123"));
+		assertEquals(account.getPassword(), "anotherterriblepassword456");
+		
+		account.changeBalance(100.00);
+		assertFalse(account.getBalance() == 0.00);
+		assertTrue(account.getBalance() == 100.00);
 	}
 	
 	/* Write more tests! */
